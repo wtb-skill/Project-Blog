8d0934320524c2ed4ebc9ed9c50b983bb559c6fc
Adam <adambaldyga@gmail.com>
2023-12-27 20:05:52 +0100

    Refactor edit_entry and create_entry into once function edit_or_create_entry and edit base.html and entry_form.html with a correct link

--- blog/routes.py
+++ blog/routes.py
@@ -12,34 +12,34 @@ def index():
     return render_template("homepage.html", all_posts=all_posts)
 
 
-@app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
+@app.route("/post/", methods=["GET", "POST"])
+@app.route("/post/<int:entry_id>", methods=["GET", "POST"])
+def edit_or_create_entry(entry_id=None):
     form = EntryForm()
     errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
+    entry = Entry.query.get_or_404(entry_id) if entry_id else None
+
+    if entry and request.method == 'GET':
+        # Pre-fill the form for editing an existing entry
+        form = EntryForm(obj=entry)
+
+    if form.validate_on_submit():
+        if not entry:
+            # Create a new entry
             entry = Entry(
                 title=form.title.data,
                 body=form.body.data,
                 is_published=form.is_published.data
             )
             db.session.add(entry)
-            db.session.commit()
             flash('Entry added!', 'success')
         else:
-            errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
-
-
-@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    form = EntryForm(obj=entry)
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
+            # Update an existing entry
             form.populate_obj(entry)
-            db.session.commit()
-        else:
-            errors = form.errors
+            flash('Entry updated!', 'success')
+
+        db.session.commit()
+    else:
+        errors = form.errors
+
     return render_template("entry_form.html", form=form, errors=errors, entry_id=entry_id)
