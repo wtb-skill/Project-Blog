f85cf14a548f8d5ffe874c9fa987c18d2e53ad5f
Adam <adambaldyga@gmail.com>
2023-12-27 19:52:38 +0100

    Add edit and pencil graphics

--- blog/routes.py
+++ blog/routes.py
@@ -29,3 +29,17 @@ def create_entry():
         else:
             errors = form.errors
     return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    form = EntryForm(obj=entry)
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            form.populate_obj(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors, entry_id=entry_id)
