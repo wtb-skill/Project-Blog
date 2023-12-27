6f0a8fa4c31e4398b2ac46b59668d6f38d8fc004
Adam <adambaldyga@gmail.com>
2023-12-27 20:54:03 +0100

    Edit forms with LoginForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,30 @@
 # blog/forms.py
 
+from werkzeug.routing import ValidationError
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField
+from wtforms import StringField, TextAreaField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
+from config import Config
 
 
 class EntryForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     body = TextAreaField('Content', validators=[DataRequired()])
     is_published = BooleanField('Is Published?')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    @staticmethod
+    def validate_username(self, field):
+        if field.data != Config.ADMIN_USERNAME:
+            raise ValidationError("Invalid username")
+        return field.data
+
+    @staticmethod
+    def validate_password(self, field):
+        if field.data != Config.ADMIN_PASSWORD:
+            raise ValidationError("Invalid password")
+        return field.data
