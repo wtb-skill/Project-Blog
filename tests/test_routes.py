3bc00698a18b298ab988168644b9b09ace0cb01a
Adam <adambaldyga@gmail.com>
2023-12-28 17:29:03 +0100

    Add simple test

---
+++ tests/test_routes.py
@@ -0,0 +1,15 @@
+from flask import url_for
+from blog import app
+import pytest
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
